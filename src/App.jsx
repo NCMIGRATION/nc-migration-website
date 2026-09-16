c700de87b5c65ca2fd4769729c8a77db35f24710
NCMIGRATION <ncmigration2930@gmail.com>
2026-09-16 18:27:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -248,8 +248,7 @@ const VISITOR_IMAGES = {
     "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?auto=format&fit=crop&w=1200&q=85",
 
   "Australia":
-  "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d5?auto=format&fit=crop&w=1200&q=85",
-  
+  "https://images.unsplash.com/photo-1523482580672-f109ba8cb9be?auto=format&fit=crop&w=1200&q=85",
   "New Zealand":
     "https://images.unsplash.com/photo-1507699622108-4be3abd695ad?auto=format&fit=crop&w=1200&q=85",
 
